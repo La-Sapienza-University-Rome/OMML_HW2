03c2d21ea6c64d7e08889b16783ed1fb78ce6747
damjimenezgu <47650665+damjimenezgu@users.noreply.github.com>
2021-12-15 10:35:03 -0500

    Add function evaluation
    Add function evaluation

--- 8_JeDiS_code/Question3/run_3_JeDiS.py
+++ 8_JeDiS_code/Question3/run_3_JeDiS.py
@@ -5,10 +5,11 @@ For making the script run:
 
 NOTE: the data is supposed to be in JeDiS_HW1/data/*.csv
 """
-
+# Import libraries
 import sys
 import os
 
+# Set directory
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
@@ -23,37 +24,50 @@ from sklearn.svm import SVC
 import time
 from sklearn.metrics import confusion_matrix
 
+# Load data set
 df = pd.read_csv('data/Letters_Q_O.csv')
 columns = list(df.columns[:-1])
 
+# Standardize features 
 df[columns] = preprocessing.StandardScaler().fit_transform(df[columns])
+
+# Get train and test sets
 train_df, test_df = train_test_split(df, test_size=0.2, random_state=1939671)
 
-# Run hyperparameter tunning for RBF Kernel
+# Define hyperparameters and kernel
 C = 5
 gamma = 0.15
 kernel = 'rbf'
 
+# Process dataframes to have them ready to apply the SVM
 X, y = process_df(train_df, ['Q', 'O'])
 X_test, y_test = process_df(test_df, ['Q', 'O'])
 
 start = time.time()
 
+# Fit SVM with the MVP method
 svm = SVMDecomposition(X, y, C=C, gamma=gamma, kernel=kernel)
 svm.fit(working_set_size=2, max_iters=5000, stop_thr=1e-5)
 
 stop = time.time()
 
+# Calculate metrics
 train_acc = svm.eval(X, y)
 test_acc = svm.eval(X_test, y_test)
 time = round((stop-start), 2)
 num_it = svm.i
 KKT = KKT_violations(svm.alpha, svm.y, svm.X, svm.w, svm.bias, svm.C)
+
+# Calculate initial and final values for the objective funtion
 alpha_init = np.zeros(len(svm.alpha))
-# fin_obj = 0.5*np.dot(np.dot(svm.alpha.T, svm.P), 
-#                         svm.alpha) + np.dot(svm.q.T, svm.alpha)
-# init_obj = 0.5*np.dot(np.dot(alpha_init.T, svm.P), 
-#                         alpha_init) + np.dot(svm.q.T, alpha_init)
+e = np.ones((X.shape[0], 1))
+
+# Calculate Q matrix to use when evaluating the objective function
+Q = np.dot((np.dot(np.diag(y), rbf(X, X, gamma))), np.diag(y))
+
+# Evaluate the objective function
+init_obj = 0.5*(np.dot(np.dot(alpha_init.T,Q),alpha_init)-np.dot(e.T,alpha_init))
+fin_obj = 0.5*(np.dot(np.dot(svm.alpha.T,Q),svm.alpha)-np.dot(e.T,svm.alpha))
 
 print('C: ', C)
 print('gamma: ', gamma)
@@ -66,3 +80,5 @@ print('Confusion Matrix: \n', matrix.reshape((2, 2)))
 print('Computational Time:', time, ' s')
 print('Number of optimizations:', num_it)
 # print(KKT)
+# print(init_obj)
+# print(fin_obj)
